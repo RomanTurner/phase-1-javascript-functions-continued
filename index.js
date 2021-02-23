72fa1f3c0a537f466a00d287354cc5ce37577755
Roman Turner <rn.turner@live.com>
2021-02-22 19:15:36 -0800

    commit tree 234b5ac206814545d7d6e865435ff5a85abe3b2d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function mondayWork(todo = "go to the office") {
+  return `This Monday, I will ${todo}.`;
+}
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function wrapAdjective(Adjective = "*") {
+    return function name(compliment = "special") {
+        return `You are ${Adjective}${compliment}${Adjective}!`;
+    };
+}
+
